0e77bfb18d1fbde5094b2ed06d9432d56e02a6a9
this-is-vigneshm <126800411+this-is-vigneshm@users.noreply.github.com>
2024-10-22 10:46:17 +0530

    d

--- src/app/service/practice.service.ts
+++ src/app/service/practice.service.ts
@@ -9,62 +9,67 @@ import { catchError, map, of, tap, throwError } from 'rxjs';
 export class PracticeService {
   users: User[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   read() {
     if (this.users.length) {
       return of(this.users);
     }
-    return this.http.get<User[]>(`/api/user`).pipe(
-      tap((users) => {
-        this.users = users;
-      }),
-      catchError(this.handleError)
-    );
+    return this.http.get<User[]>(`/api/user`)
+      .pipe(
+        tap((users) => {
+          this.users = users;
+        }),
+        catchError(this.handleError)
+      );
   }
 
   readOne(id: string | null) {
-    return this.read().pipe(
-      map((users) => {
-        const user = users.find((user: User) => user.id === id);
-        if (user) {
-          return user;
-        }
-        return { name: '', age: 0, mail: '' };
-      })
-    );
+    return this.read()
+      .pipe(
+        map((users) => {
+          const user = users.find((user: User) => user.id === id);
+          if (user) {
+            return user;
+          }
+          return { name: '', age: 0, mail: '' };
+        })
+      );
   }
 
   createUser(payLoad: User) {
-    return this.http.post<User>(`api/user`, payLoad).pipe(
-      tap((user) => {
-        this.users = [...this.users, user];
-      }),
-      catchError(this.handleError)
-    );
+    return this.http.post<User>(`api/user`, payLoad)
+      .pipe(
+        tap((user) => {
+          this.users = [...this.users, user];
+        }),
+        catchError(this.handleError)
+      );
   }
 
   updateUser(payLoad: User) {
-    return this.http.put<User>(`api/user/${payLoad.id}`, payLoad).pipe(
-      tap((user) => {
-        this.users = this.users.map((item: User) => {
-          if (item.id === payLoad.id) {
-            return user;
-          }
-          return item;
-        });
-      }),
-      catchError(this.handleError)
-    );
+    return this.http.put<User>(`api/user/${payLoad.id}`, payLoad)
+      .pipe(
+        tap((user) => {
+          this.users = this.users.map((item: User) => {
+            if (item.id === payLoad.id) {
+              return user;
+            }
+            return item;
+          });
+        }),
+        catchError(this.handleError)
+      );
   }
 
   deleteUser(payLoad: User) {
-    return this.http.delete<User>(`api/user/${payLoad.id}`).pipe(
-      tap(() => {
-        this.users = this.users.filter((item: User) => item.id !== payLoad.id);
-      }),
-      catchError(this.handleError)
-    );
+    return this.http.delete<User>(`api/user/${payLoad.id}`)
+      .pipe(
+        tap(() => {
+          this.users = this.users.filter((item: User) => item.id !== payLoad.id);
+        }),
+        catchError(this.handleError)
+      );
   }
 
   handleError(err: HttpErrorResponse) {
